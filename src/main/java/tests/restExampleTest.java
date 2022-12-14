3ca9b509498150f5068f8dd146f219a55e755c81
Catalin Ursu <catalin@academicmerit.com>
2022-12-14 15:18:46 +0100

    first commit

---
+++ src/main/java/tests/restExampleTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+import io.restassured.response.Response;
+
+
+public class restExampleTest {
+
+	@Test
+	public void postAtoDo() {
+		Response response;
+	}
+	
+}
