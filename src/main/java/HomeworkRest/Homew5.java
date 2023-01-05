01b0678216a2ce1c3007bc1d92f0b0f5b42b6187
Catalin Ursu <catalin@academicmerit.com>
2023-01-05 11:26:07 +0100

    tema5

--- src/main/java/HomeworkRest/Homew5.java
+++ src/main/java/HomeworkRest/Homew5.java
@@ -1,5 +1,74 @@
 package HomeworkRest;
 
-public class Homew5 {
+import static org.testng.Assert.assertEquals;
 
+import org.apache.http.HttpStatus;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import io.restassured.response.Response;
+import utils.BaseComponent;
+
+public class Homew5 extends BaseComponent{
+
+	String id;
+	String Fake_name;
+	
+	JSONObject body =  new JSONObject();
+	
+	@Test(priority=1)
+	public void postCustomer() {
+		
+		Faker faker = new Faker();
+		
+		Fake_name = faker.name().firstName();
+		body.put("name", Fake_name);
+		body.put("trips", 250);
+		body.put("airline", 1981);
+		
+		Response response = doPostRequest("v1/passenger", body.toJSONString(), 200);
+		assertEquals(response.jsonPath().getString("name"), Fake_name);
+		id = response.jsonPath().getString("_id");
+		
+	}
+	
+	@Test(priority=2)
+	public void getCustomer() {
+		
+		Response response = doGetRequest("v1/passenger/", id, 200);
+		assertEquals(response.jsonPath().get("airline.name[0]"), "Tarom");
+		
+	}
+	
+	@Test(priority=3)
+	public void putCustomer() {
+		
+		body.put("trips", 300);
+		body.put("airline", 1);
+		
+		Response response = doPutRequest("v1/passenger/", id, body.toJSONString(), 200);
+		System.out.println(id);
+		assertEquals(response.jsonPath().getString("message"), "Passenger data put successfully completed.");
+		
+	}
+	
+	@Test(priority=4)
+	public void assertUpdate() {
+		
+		Response response = doGetRequest("v1/passenger/", id, 200);
+		assertEquals(response.jsonPath().get("airline.name[0]"), "Quatar Airways");
+		assertEquals(response.jsonPath().get("trips"), 300);
+		assertEquals(response.jsonPath().get("name"), Fake_name);
+		
+	}
+	
+	@Test(priority=5)
+	public void deleteCustomer() {
+		
+		Response response = doDeleteRequest("v1/passenger/", id, 200);
+		assertEquals(response.jsonPath().getString("message"), "Passenger data deleted successfully.");
+	}
+	
 }
--- src/main/java/utils/BaseComponent.java
+++ src/main/java/utils/BaseComponent.java
@@ -16,10 +16,13 @@ public class BaseComponent {
 	@BeforeClass
 	public void setup() {
 		
-		RestAssured.baseURI = "https://keytrcrud.herokuapp.com/";
+		//RestAssured.baseURI = "https://keytrcrud.herokuapp.com/";
 		
 		//Needed for Homework3
 		//RestAssured.baseURI = "https://fakerestapi.azurewebsites.net/";
+		
+		//Needed for Homework5
+		RestAssured.baseURI = "https://api.instantwebtools.net/";
 
 	}
 	
@@ -68,6 +71,22 @@ public class BaseComponent {
 					extract().response();
 			
 		return result;
+	}
+	
+	public static Response doPutRequest(String path, String id, String body, int statusCode) {
+		
+		Response result = 
+				given().
+					contentType(ContentType.JSON).
+					body(body).
+				when().
+					put(path + id).
+				then().
+					statusCode(statusCode).
+					extract().response();
+			
+		return result;
+		
 	}
 	
 	public static Response doDeleteRequest(String path, String id, int statusCode) {
